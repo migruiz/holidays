0f0ddf459335f810ef557a8e76631ec926e5dad0
Miguel <miguel@gmail.com>
2024-01-15 15:44:03 +0000

    error and hours

--- App/app.js
+++ App/app.js
@@ -12,7 +12,7 @@ global.mtqqLocalPath = 'mqtt://192.168.0.11';
 const START_MAX_DELAY = 15 * 60 * 1000
 const KEEP_ON_MIN = 20 * 60 * 1000
 const MAX_ON = 60 * 60 * 1000 - (START_MAX_DELAY + KEEP_ON_MIN)
-const operationHours = [2, 7, 15, 16, 17, 18, 19, 20, 21, 22]
+const operationHours = [2, 7, 16, 17, 18, 19, 20, 21, 22]
 
 
 const getHouseAreaStream = () => {
@@ -46,11 +46,9 @@ const houseAreas = [
     area: 'kitchen',
     stream: getHouseAreaStream(),
     onAction: async () => {
-      console.log("kitchen on action")
         (await mqtt.getClusterAsync()).publishMessage('kitchen/lights', '1000');
     },
     offAction: async () => {
-      console.log("kitchen off action")
         (await mqtt.getClusterAsync()).publishMessage('kitchen/lights', '0');
     }
   },
@@ -58,12 +56,10 @@ const houseAreas = [
     area: 'livingroom',
     stream: getHouseAreaStream(),
     onAction: async () => {
-      console.log("livingroom on action")
         (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x2c1165fffed897d3/set', JSON.stringify({ brightness: 254 }));
       (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x2c1165fffed8947e/set', JSON.stringify({ brightness: 254 }));
     },
     offAction: async () => {
-      console.log("livingroom off action")
         (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x2c1165fffed897d3/set', JSON.stringify({ brightness: 0 }));
       (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x2c1165fffed8947e/set', JSON.stringify({ brightness: 0 }));
     }
@@ -72,12 +68,10 @@ const houseAreas = [
     area: 'aleroom',
     stream: getHouseAreaStream(),
     onAction: async () => {
-      console.log("aleroom on action")
         (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x0c4314fffe20d4f8/set', JSON.stringify({ state: 'ON' }));
       (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x385b44fffee7a042/set', JSON.stringify({ state: 'ON' }));
     },
     offAction: async () => {
-      console.log("aleroom off action")
         (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x0c4314fffe20d4f8/set', JSON.stringify({ state: 'OFF' }));
       (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x385b44fffee7a042/set', JSON.stringify({ state: 'OFF' }));
     }
@@ -86,12 +80,10 @@ const houseAreas = [
     area: 'masterroom',
     stream: getHouseAreaStream(),
     onAction: async () => {
-      console.log("masterroom on action")
         (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x04cd15fffe9d3c8a/set', JSON.stringify({ brightness: 254 }));
       (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x04cd15fffe8a196d/set', JSON.stringify({ brightness: 254 }));
     },
     offAction: async () => {
-      console.log("masterroom off action")
         (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x04cd15fffe9d3c8a/set', JSON.stringify({ brightness: 0 }));
       (await mqtt.getClusterAsync()).publishMessage('zigbee2mqtt/0x04cd15fffe8a196d/set', JSON.stringify({ brightness: 0 }));
     }
